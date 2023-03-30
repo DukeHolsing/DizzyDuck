adbe6b3c7c12ba2047dd5a693ff42d43f28bb8af
Duke Holsing <dh021804@gmail.com>
2023-03-30 08:25:37 -0400

    Initial Commit

---
+++ Duck/DuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuckApp.swift
+//  Duck
+//
+//  Created by Duke on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
